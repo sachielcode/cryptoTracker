86f06b39c9bd9eed8921de70d3153817ae65a554
José Luis Ramírez Morales <sach13lrm@gmail.com>
2020-09-15 07:02:16 -0500

    Added first tab

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import CoinsStack from 'cryptoTracker/src/components/coins/CoinsStack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Colors from 'cryptoTracker/src/resources/colors';
+
+const Tabs = createBottomTabNavigator();
 
 const App = () => {
 
   return (
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator
+	tabBarOptions={{
+	  tintColor: "#fefefe",
+	  style: {
+	   backgroundColor: Colors.blackPearl
+	  }
+	}}
+      >
+	<Tabs.Screen
+	  name="Coins"
+	  component={CoinsStack} 
+	  options={{
+	  tabBarIcon: ({ size, color }) => (
+	      <Image 
+		style={{ tintColor: color, width: size, height: size }}
+		source={ require('cryptoTracker/src/assets/bank.png') } 
+	      />
+	    )
+	  }}
+	/>
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
